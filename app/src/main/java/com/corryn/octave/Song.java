2cab74fc3b57386c19b571be9a94364dd65513b2
Adam Tonet <adamtonet@gmail.com>
2023-12-25 12:26:46 -0500

    Create fresh project and transplant Octave code+resources to skirt ten years of Gradle update headaches

---
+++ app/src/main/java/com/corryn/octave/Song.java
@@ -0,0 +1,24 @@
+package com.corryn.octave;
+
+public class Song {
+
+    private long id;
+    private long albumId;
+    private String title;
+    private String artist;
+    private String data;
+
+    public Song(long songID, long aId, String songTitle, String songArtist, String songData) {
+        id = songID;
+        albumId = aId;
+        title = songTitle;
+        artist = songArtist;
+        data = songData;
+    }
+
+    public long getID(){return id;}
+    public long getAlbumID(){return albumId;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+    public String getData(){return data;}
+}
